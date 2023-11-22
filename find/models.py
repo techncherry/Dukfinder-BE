b6bf00079e299a7849e47f38ddf98d9bcdfa6ae4
hyunnjjung <hyunnjjung@gmail.com>
2023-11-22 21:21:14 +0900

    FindPost 모델 생성

---
+++ find/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+
+class Post(models.Model):
+    title = models.CharField(max_length=30, blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    content = models.TextField()
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: {self.author}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
