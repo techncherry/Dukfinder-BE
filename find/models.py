5e958bce2c0ce7244c214890655c600e61ae31e9
hyunnjjung <hyunnjjung@gmail.com>
2023-11-22 21:23:24 +0900

    FindCategory생성

--- find/models.py
+++ find/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 import os
+class FindCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class FindPost(models.Model):
     title = models.CharField(max_length=30, blank=True)
     hook_text = models.CharField(max_length=100, blank=True)
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(FindCategory, null=True, blank=True, on_delete=models.SET_NULL)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
