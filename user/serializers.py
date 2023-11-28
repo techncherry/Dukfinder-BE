4ba1ed62459d4350c194d249bf28c6073b7783d3
Nayoung Kim <kimnadon@naver.com>
2023-11-28 23:59:02 +0900

    비밀변호 변경 추가

--- user/serializers.py
+++ user/serializers.py
@@ -1,111 +1,90 @@
-from django.contrib.auth.models import User  # User 모델
-# Django의 기본 패스워드 검증 도구
-from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth import authenticate
+from django.contrib.auth.models import User  # 장고의 기본 User 모델 사용
+from django.contrib.auth.password_validation import validate_password  # Django의 기본 pw 검증 도구
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token  # Token 모델
 from rest_framework.validators import UniqueValidator  # 이메일 중복 방지를 위한 검증 도구
+from django.contrib.auth import authenticate # Django의 기본 authenticate 함수 -> 우리가 설정한 DefaultAuthBackend인 TokenAuth 방식으로 유저를 인증해준다.
 
-# 이메일 인증
-from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes, force_str
-from django.utils.http import urlsafe_base64_encode
-from rest_framework_jwt.settings import api_settings
-from rest_framework.authtoken.models import Token
-
-# from .models import Profile
-
-
-JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
-JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
-
+from .models import Profile
 
 # 회원가입 시리얼라이저
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        help_text="이메일(example@duksung.ac.kr)",
-        required=True,  # 필수 입력 설정
-        validators=[UniqueValidator(queryset=User.objects.all())],  # 이메일 중복 검증
+        required=True, # 필수 항목 의미
+        validators=[UniqueValidator(queryset=User.objects.all())],  # 이메일에 대한 중복 검증
     )
-
-    # 이메일이 @duksung.ac.kr로 끝나는지 확인
-    def validate_email(self, value):
-        if not value.endswith('@duksung.ac.kr'):
-            raise serializers.ValidationError("이메일은 @duksung.ac.kr로 끝나야 합니다.")
-        return value
-
-    # 비밀번호 1차 입력
     password = serializers.CharField(
-        help_text="비밀번호",
-        write_only=True,  # 출력 못하게
+        write_only=True, # 비밀번호는 쓰기만 되도록
         required=True,
-        validators=[validate_password],  # 비밀번호 검증
+        validators=[validate_password],  # 비밀번호에 대한 검증
     )
-
-    # 비밀번호 2차 입력
-    password2 = serializers.CharField(
-        help_text="비밀번호 재입력", write_only=True, required=True, )
+    password2 = serializers.CharField(  # 비밀번호 확인을 위한 필드
+        write_only=True,
+        required=True,
+    )
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password2')
 
-    # 비밀번호 일치 여부 확인
-    def validate(self, data):
+    def validate(self, data):  # password과 password2의 일치 여부 확인
         if data['password'] != data['password2']:
             raise serializers.ValidationError(
-                {"password": "비밀번호를 다시 입력하세요."})
+                {"password": "Password fields didn't match."})
+        """기록하레이에서 여기만 추가"""
+        if not data.get("email", "").endswith("@duksung.ac.kr"):
+            raise serializers.ValidationError(
+                detail={"error": "덕성 이메일만 가입할 수 있습니다."}
+            )
 
         return data
 
-        # CREATE 요청에 대해 create 메소드를 오버라이딩, 유저를 생성하고 토큰을 생성하게 함.
-
     def create(self, validated_data):
+        # CREATE 요청에 대해 create 메서드를 오버라이딩하여, 유저를 생성하고 토큰도 생성하게 해준다.
         user = User.objects.create_user(
-            username=validated_data['username'],  # username 필드에 저장
-            email=validated_data['email'],  # email 필드에 저장
+            username=validated_data['username'],
+            email=validated_data['email'],
         )
 
-        user.set_password(validated_data['password'])  # password 필드에 저장
-        user.is_active = False  # 인증 전 기본값
-        user.save()  # 변경된 사용자 정보 저장
-
-        payload = JWT_PAYLOAD_HANDLER(user)
-        jwt_token = JWT_ENCODE_HANDLER(payload)
-        message = render_to_string('user/user_activate_email.html', {
-            'user': user,
-            'domain': 'localhost:8000',
-            'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
-            'token': jwt_token,
-        })
-        mail_subject = '[DukFinder] 회원가입 인증 메일입니다'
-        to_email = user.email
-        email = EmailMessage(mail_subject, message, to=[to_email])
-        email.send()
-
-        token = Token.objects.create(user=user)  # 사용자에 대한 새로운 토큰 생성
+        user.set_password(validated_data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
         return user
-
 
 # 로그인 시리얼라이저
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
+    # write_only=True 옵션을 통해 클라이언트->서버의 역직렬화는 가능하지만, 서버->클라이언트 방향의 직렬화는 불가능하도록 해준다.
+
     def validate(self, data):
         user = authenticate(**data)
         if user:
-            token = Token.objects.get(user=user)
+            token = Token.objects.get(user=user)  # 해당 유저의 토큰을 불러옴
             return token
-        raise serializers.ValidationError(
-            {"error": "잘못된 입력입니다."})
+        raise serializers.ValidationError(  # 가입된 유저가 없을 경우
+            {"error": "가입된 회원이 아닙니다."}
+        )
 
-
-"""
+# 프로필 사진 시리얼라이저
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("nickname", "position", "subjects", "image")
-"""
+        fields = ("image", ) # 이거 , 넣는 거 중괄호 맞나?
+
+# 유저 이름, 이메일
+class UserinfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+# 비밀번호
+class PasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("old_password", "new_password")
--- user/views.py
+++ user/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer, UserinfoSerializer
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer, UserinfoSerializer, PasswordSerializer
 from .models import Profile
 from .permissions import CustomReadOnly
 from django.contrib.auth import logout
+from rest_framework.permissions import IsAuthenticated
 
 # 로그인
 from django.contrib.auth.models import User
@@ -59,5 +58,30 @@ class UserinfoView(generics.GenericAPIView):
         data = {'success': '로그아웃 실패'}
         return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
 
+class PasswordView(generics.UpdateAPIView):
+    serializer_class = PasswordSerializer
+    permission_classes = (IsAuthenticated,)
 
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            old_password = serializer.validated_data['old_password']
+            new_password = serializer.validated_data['new_password']
+
+            # Check if the old password is correct
+            if not user.check_password(old_password):
+                return Response({'detail': 'Old password is incorrect.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Set the new password and save the user object
+            user.set_password(new_password)
+            user.save()
+
+            return Response({'detail': 'Password changed successfully.'}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
