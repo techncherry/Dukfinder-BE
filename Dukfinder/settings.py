b0148e604a4dc46b4ea4d73caf187384b11296ca
hyunnjjung <hyunnjjung@gmail.com>
2023-12-01 01:15:51 +0900

    find앱 생성

--- Dukfinder/settings.py
+++ Dukfinder/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'notice'
+    'notice',
+    'find',
+    'lost',
 ]
 
 MIDDLEWARE = [
--- Dukfinder/urls.py
+++ Dukfinder/urls.py
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('find.urls')),
     path('', include('notice.urls')),
+    path('', include('lost.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
