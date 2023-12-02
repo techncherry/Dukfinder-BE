f59caa879e82a97c3ae106f510807d43ba88c8cf
hyunnjjung <141250069+hyunnjjung@users.noreply.github.com>
2023-12-02 19:45:15 +0900

    Update models.py

--- notice/models.py
+++ notice/models.py
@@ -7,14 +7,14 @@ class NoticePost(models.Model): # 공지사항 포스트 모델
 
     content = models.TextField() # 내용
 
-    notice_image = models.ImageField(upload_to='notice/images/%Y/%m/%d/', blank=True)
-    top_fixed = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
+    notice_image = models.ImageField(upload_to='notice/images/%Y/%m/%d/', blank=True) # 공지사항 포스트 이미지
+    top_fixed = models.BooleanField(default=False) # 상단고정 여부
+    created_at = models.DateField(auto_now_add=True) # 작성일
 
 
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # 작성자
 
-    view_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0) #조회수
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
